66613f85c7e94a8f8505910ead325914ec6869ef
DIOGO SILVA <56855879+Diogojackson@users.noreply.github.com>
2022-08-04 20:00:00 -0300

    update colorspec

---
+++ colorspec.Rcheck/00_pkg_src/colorspec/R/fixspec.R
@@ -0,0 +1,19 @@
+#' fixspec function
+#'
+#' This function convert to rspecdata, normalize, fix negative values and applies smoothing. Please, see procspec and as.rspec functions from 'pavo' package
+#'
+#' @param rspecdata A data frame, possibly of class rspec, which contains a column containing a wavelength range, named 'wl', and spectra data in remaining columns.
+#'
+#'
+#' @examples
+#' data(refletancias)
+#' refletancias <- fixspec(refletancias)
+#'
+#' @export
+fixspec <- function(rspecdata){
+
+  reflet <- pavo::procspec(rspecdata, opt = 'smooth',fixneg = 'addmin',span = 0.25, bins = 5)
+  reflet <- pavo::as.rspec(reflet,lim=c(300,700))
+
+  print(reflet)
+}
---
+++ colorspec/R/refletancias.R
@@ -0,0 +1,14 @@
+#' @title fiddler crab reflectances
+#'
+#' @description a \code{data.frame} that
+#' \format a \code{data.frame} with 7 columns
+#' \describe{
+#' \item{wl}{Wavelenght column}
+#' \item{01_m_c}{carapace reflectance of male number 1}
+#' \item{01_m_q}{claw reflectance of male number 1}
+#' \item{02_m_c}{carapace reflectance of male number 2}
+#' \item{02_m_q}{claw reflectance of male number 2}
+#' \item{03_m_c}{carapace reflectance of male number 3}
+#' \item{03_m_q}{claw reflectance of male number 3}
+#' }
+"refletancias"
