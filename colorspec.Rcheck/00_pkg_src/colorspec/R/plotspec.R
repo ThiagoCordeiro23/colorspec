86d2e60010726b576afc8315a4ddfa56c4269a2a
DIOGO SILVA <56855879+Diogojackson@users.noreply.github.com>
2022-08-04 23:56:06 -0300

    correcting errors

---
+++ colorspec.Rcheck/00_pkg_src/colorspec/R/plotspec.R
@@ -0,0 +1,25 @@
+#' plot average spectra
+#'
+#' Calc and plot the average (central line) and standard deviation (shadow area) of a given spectra, according to a vector of identities. Please, see aggplot function from 'pavo' package.
+#'
+#' @param rspecdata A data frame, possibly of class rspec, which contains a column containing a wavelength range, named 'wl', and spectra data in remaining columns.
+#' @param ... you can use the arguments of the pavo::aggplot (ex: ylim = c(0, 100), main = "aggplot")
+#'
+#' @examples
+#' #example 1 ## not run
+#' data(leptodactyla)
+#' refletancias <- fixspec(leptodactyla)
+#' plotspec(leptodactyla)
+#'
+#' @return It is a plot.
+#'
+#' @export
+plotspec <- function(rspecdata,...){
+
+  bysic <- gsub("[0-9]*", "", names(rspecdata)[-1])
+  plot  <- pavo::aggplot(rspecdata,
+                         bysic,
+                         alpha  = 0.3,
+                         legend = T,...)
+  return(plot)
+}
---
+++ colorspec.Rcheck/00_pkg_src/colorspec/R/vis-bluetit.R
@@ -0,0 +1,41 @@
+#' Bluetit vismodel
+#'
+#' Calculate quantum catch (Qi), chromatic and achromatic contrast, given in
+#' JND units.
+#' pavo::vismodel Parameters: qcatch = "Qi",visual = "bluetit", achromatic = "bt.dc",illum = "D65",trans = "bluetit", scale = 1, relative = FALSE.
+#' pavo::coldist parameters: qcatch = NULL, noise = "neural", n = c(1,1.9,2.7,2.7), weber.ref ='longest', weber = 0.1
+#' To set the parameters manualy see vismodel() and coldist() functions of 'pavo' package.
+#'
+#' @param rspecdata A data frame, possibly of class rspec, which contains a column containing a wavelength range, named 'wl', and spectra data in remaining columns.
+#' @param background choice one column spectrum data to be the background in RNL model.
+#'
+#' @examples
+#' #example ## not run
+#' data(leptodactyla)
+#' leptodactyla <- fixspec(leptodactyla)
+#' vis.bt <- vis.bluetit(leptodactyla, background = "X00_background")
+#'
+#' @export
+vis.bluetit <- function(rspecdata, background,...){
+
+  #vismodel
+  QI_tit   <- pavo::vismodel(rspecdata, qcatch = "Qi",visual = "bluetit", achromatic = "bt.dc",illum = "D65",trans = "bluetit", scale = 1, relative = FALSE)
+  JND_tit  <- pavo::coldist(QI_tit, qcatch = NULL, noise = "neural", subset = background, achro=TRUE, n = c(1,1.9,2.7,2.7), weber.ref='longest', weber = 0.1, weber.achro = TRUE)
+
+  QI_tit <- QI_tit %>%
+    tibble::rownames_to_column(var = "ID") %>%
+    dplyr::filter(ID != background)
+
+  result <- dplyr::bind_cols(summary_cor, QI_tit, JND_tit)
+
+  result <- dplyr::select(result, -patch1, -patch2) %>%
+    dplyr::rename(chromatic_contrast = dS,
+           achromatic_contrast = dL,
+           luminance = lum,
+           saturation = S8,
+           mean_brightness = B2,
+           hue = H1)
+
+  return(result)
+
+}
