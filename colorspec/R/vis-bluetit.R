f678e14fe77c7e842bc665e252047683a4f439a4
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-01-19 13:59:11 +1000

    update function bluetit
    add argumento illum

--- colorspec/R/vis-bluetit.R
+++ colorspec/R/vis-bluetit.R
@@ -8,18 +8,19 @@
 #'
 #' @param rspecdata A data frame, possibly of class rspec, which contains a column containing a wavelength range, named 'wl', and spectra data in remaining columns.
 #' @param background choice one column spectrum data to be the background in RNL model.
+#' @param illum you can chose the followings illuminantes: "D65", "bluesky", and "forestshade".
 #'
 #' @examples
 #' #example ## not run
 #' data(leptodactyla)
 #' leptodactyla <- fixspec(leptodactyla)
-#' vis.bt <- vis.bluetit(leptodactyla, background = "X00_background")
+#' vis.bt <- vis.bluetit(leptodactyla, background = "X00_background", illum = "D65")
 #'
 #' @export
-vis.bluetit <- function(rspecdata, background){
+vis.bluetit <- function(rspecdata, background, illum){
 
   #vismodel
-  QI_tit   <- pavo::vismodel(rspecdata, qcatch = "Qi",visual = "bluetit", achromatic = "bt.dc",illum = "D65",trans = "bluetit", scale = 1, relative = FALSE)
+  QI_tit   <- pavo::vismodel(rspecdata, qcatch = "Qi",visual = "bluetit", achromatic = "bt.dc",illum = illum,trans = "bluetit", scale = 1, relative = FALSE)
   JND_tit  <- pavo::coldist(QI_tit, qcatch = NULL, noise = "neural", subset = background, achro=TRUE, n = c(1,1.9,2.7,2.7), weber.ref='longest', weber = 0.1, weber.achro = TRUE)
 
   QI_tit <- QI_tit %>%
